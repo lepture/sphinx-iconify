e3fd5f47b6f23557c1b94e11b58dc266a372d638
Hsiaoming Yang <me@lepture.com>
2025-07-14 15:51:16 +0900

    docs: init

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Sphinx Iconify"
+copyright = "2025, Hsiaoming Yang"
+author = "Hsiaoming Yang"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx_iconify",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "shibuya"
+
+html_theme_options = {
+    "page_layout": "simple",
+}
+
+# shibuya theme has built-in <iconify-icon>
+iconify_script_url = ""
---
+++ docs/index.rst
@@ -0,0 +1,12 @@
+Sphinx Iconify
+==============
+
+Install
+-------
+
+Usage
+-----
+
+:iconify:`simple-icons:github`
+
+:iconify:`simple-icons:github width=24px height=24px`
